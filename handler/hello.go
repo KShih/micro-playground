c53a892a9e4ed870e6765fc2d93262e76f7a8531
KShih <kontonsine@gmail.com>
2020-09-17 14:44:43 +0800

    [modify] modify service call another service memory leak

--- handler/hello.go
+++ handler/hello.go
@@ -7,11 +7,11 @@ import (
 	nextProto "micro-playground/proto/nextHelloWorld"
 
 	proto "micro-playground/proto/helloworld"
-
-	"github.com/micro/go-micro/v2"
 )
 
-type GreeterServiceHandler struct{}
+type GreeterServiceHandler struct {
+	NextHelloClient nextProto.NextGreeterService
+}
 
 func (g *GreeterServiceHandler) Hello(ctx context.Context, req *proto.HelloRequest, rsp *proto.HelloResponse) error {
 	rsp.Greeting = " 你好, " + req.Name
@@ -19,21 +19,12 @@ func (g *GreeterServiceHandler) Hello(ctx context.Context, req *proto.HelloReque
 }
 
 func (g *GreeterServiceHandler) NextHello(ctx context.Context, req *proto.HelloRequest, rsp *proto.HelloResponse) error {
-	service := micro.NewService(micro.Name("Greeter.Client"))
 
-	service.Init()
-
-	// finding another service
-	nextGreeter := nextProto.NewNextGreeterService("NextGreeter", service.Client())
-	nextRsp, err := nextGreeter.Hello(context.TODO(), &nextProto.HelloRequest{Name: req.Name}) // take the input and pass to another service
+	nextRsp, err := g.NextHelloClient.Hello(ctx, &nextProto.HelloRequest{Name: req.Name}) // take the input and pass to another service
 	rsp.Greeting = nextRsp.Greeting
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	// Print response
-	fmt.Println(rsp.Greeting)
-
-	// rsp.Greeting =
 	return nil
 }
--- main.go
+++ main.go
@@ -4,18 +4,23 @@ import (
 	"fmt"
 	"micro-playground/handler"
 	proto "micro-playground/proto/helloworld"
+	nextProto "micro-playground/proto/nextHelloWorld"
 
+	grmon "github.com/bcicen/grmon/agent"
 	"github.com/micro/go-micro/v2"
 )
 
 func main() {
+	grmon.Start()
 	service := micro.NewService(
 		micro.Name("Greeter"),
 	)
 
 	service.Init()
 
-	proto.RegisterGreeterHandler(service.Server(), new(handler.GreeterServiceHandler))
+	proto.RegisterGreeterHandler(service.Server(), &handler.GreeterServiceHandler{
+		NextHelloClient: nextProto.NewNextGreeterService("NextGreeter", service.Client())
+	})
 
 	if err := service.Run(); err != nil {
 		fmt.Println(err)
